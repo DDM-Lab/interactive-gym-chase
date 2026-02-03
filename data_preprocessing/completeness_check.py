e25d5ba30f278c99ce15bf2561afa92def8ff9c3
Grace Roessling <grace.roessling19@gmail.com>
2026-02-03 17:17:14 -0500

    refined quitter logic after finding new examples in aws

--- data_preprocessing/completeness_check.py
+++ data_preprocessing/completeness_check.py
@@ -2,8 +2,8 @@ from pathlib import Path
 import csv
 from quit_initiator_detector import detect_quit_initiator
 
-data_dir = r"G:\.shortcut-targets-by-id\1n7peZVybcw0B7smQ0VFfiXWcIbYjxZ96\2025ControllableCollaborationChaseGrace\Experiments\2025-ControllableCollaboration-Human Speed-HH\Data\FullRuns\human-only-data_run_2"
-
+#data_dir = r"G:\.shortcut-targets-by-id\1n7peZVybcw0B7smQ0VFfiXWcIbYjxZ96\2025ControllableCollaborationChaseGrace\Experiments\2025-ControllableCollaboration-Human Speed-HH\Data\FullRuns\human-only-data_run_2"
+data_dir = r"G:\.shortcut-targets-by-id\1n7peZVybcw0B7smQ0VFfiXWcIbYjxZ96\2025ControllableCollaborationChaseGrace\Experiments\2025-ControllableCollaboration-Human Speed-HH\Data\FullRuns\human-only-run-1-aws"
 # Parameter: "human-only", "AI-only", or "Human-AI"
 experiment_type = "human-only"
 suffix = "hh" if experiment_type.lower() == "human-only" else "sp"
