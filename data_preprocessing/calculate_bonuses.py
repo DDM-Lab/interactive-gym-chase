ef8b62b988b0db0acdfd61a17791afcc5c7dbe0f
Grace Roessling <grace.roessling19@gmail.com>
2026-02-04 10:48:51 -0500

    processed 2.2.26 experiment run,only modified file paths and ran the scripts

--- data_preprocessing/calculate_bonuses.py
+++ data_preprocessing/calculate_bonuses.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 
 # Configuration
 experiment_type = "human-only"  # Change to "AI-only" or "Human-AI" as needed
-agg_data_dir = r"C:\Users\groessli\Documents\GitHub\interactive-gym-chase\data_preprocessing\human_only\aggregated_data\run_1_aws"
-fig_dir = r"C:\Users\groessli\Documents\GitHub\interactive-gym-chase\data_preprocessing\human_only\aggregated_data\run_1_aws\generated_figs"
-aligned_team_data_dir = r"C:\Users\groessli\Documents\GitHub\interactive-gym-chase\data_preprocessing\human_only\aggregated_data\run_1_aws\aligned_team_data"
+agg_data_dir = r"C:\Users\groessli\Documents\GitHub\interactive-gym-chase\data_preprocessing\human_only\aggregated_data\run_2"
+fig_dir = r"C:\Users\groessli\Documents\GitHub\interactive-gym-chase\data_preprocessing\human_only\aggregated_data\run_2\generated_figs"
+aligned_team_data_dir = r"C:\Users\groessli\Documents\GitHub\interactive-gym-chase\data_preprocessing\human_only\aggregated_data\run_2\aligned_team_data"
 
 # Category folder names
 CATEGORY_FOLDERS = {
--- data_preprocessing/data_aggregation.py
+++ data_preprocessing/data_aggregation.py
@@ -5,8 +5,9 @@ import csv
 from quit_initiator_detector import detect_quit_initiator
 
 #data_dir = r"G:\.shortcut-targets-by-id\1n7peZVybcw0B7smQ0VFfiXWcIbYjxZ96\2025ControllableCollaborationChaseGrace\Experiments\2025-ControllableCollaboration-Human Speed-HH\Data\FullRuns\human-only-data_run_1"
-data_dir = r"G:\.shortcut-targets-by-id\1n7peZVybcw0B7smQ0VFfiXWcIbYjxZ96\2025ControllableCollaborationChaseGrace\Experiments\2025-ControllableCollaboration-Human Speed-HH\Data\FullRuns\human-only-run-1-aws"
-agg_data_dir = r"C:\Users\groessli\Documents\GitHub\interactive-gym-chase\data_preprocessing\human_only\aggregated_data\run_1_aws"
+#data_dir = r"G:\.shortcut-targets-by-id\1n7peZVybcw0B7smQ0VFfiXWcIbYjxZ96\2025ControllableCollaborationChaseGrace\Experiments\2025-ControllableCollaboration-Human Speed-HH\Data\FullRuns\human-only-run-1-aws"
+agg_data_dir = r"C:\Users\groessli\Documents\GitHub\interactive-gym-chase\data_preprocessing\human_only\aggregated_data\run_2"
+data_dir = r"G:\.shortcut-targets-by-id\1n7peZVybcw0B7smQ0VFfiXWcIbYjxZ96\2025ControllableCollaborationChaseGrace\Experiments\2025-ControllableCollaboration-Human Speed-HH\Data\FullRuns\human-only-data_run_2"
 
 # Parameter: "human-only", "AI-only", or "Human-AI"
 experiment_type = "human-only"
